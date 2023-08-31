ffe06b5455571086e5f224813aa41830f2ec6236
Muhammad Qasim <qasimrafiq2019@gmail.com>
2023-08-31 14:01:08 +0500

    Carousel

--- src/app/[products]/image.tsx
+++ src/app/[products]/image.tsx
@@ -17,19 +17,31 @@ export default function Image2({ item }: { item: any }) {
             onClick={() => setData(item.image)}
           />
           <img
-            className="h-[60px] min-w-[60px] md:min-w-[100px] md:h-[100px] cursor-pointer"
+            className={`h-[60px] min-w-[60px] md:min-w-[100px] md:h-[100px] cursor-pointer ${
+              urlForImage(item.image).url() === urlForImage(item.image2).url()
+                ? "opacity-0"
+                : "opacity-100"
+            }`}
             src={urlForImage(item.image2).url()}
             alt=""
             onClick={() => setData(item.image2)}
           />
           <img
-            className="h-[60px] min-w-[60px] md:min-w-[100px] md:h-[100px] cursor-pointer"
+            className={`h-[60px] min-w-[60px] md:min-w-[100px] md:h-[100px] cursor-pointer ${
+              urlForImage(item.image).url() === urlForImage(item.image3).url()
+                ? "opacity-0"
+                : "opacity-100"
+            }`}
             src={urlForImage(item.image3).url()}
             alt=""
             onClick={() => setData(item.image3)}
           />
           <img
-            className="h-[60px] min-w-[60px] md:min-w-[100px] md:h-[100px] cursor-pointer"
+            className={`h-[60px] min-w-[60px] md:min-w-[100px] md:h-[100px] cursor-pointer ${
+              urlForImage(item.image).url() === urlForImage(item.image4).url()
+                ? "opacity-0"
+                : "opacity-100"
+            }`}
             src={urlForImage(item.image4).url()}
             alt=""
             onClick={() => setData(item.image4)}
